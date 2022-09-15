edfbab4668db2b1bad446c9fadedceab2dd4142f
branko <brankodevstar@outlook.com>
2022-09-15 12:50:40 -0700

    member benefits screen init

--- src/screens/benefits_map/memberBenefits.js
+++ src/screens/benefits_map/memberBenefits.js
@@ -14,8 +14,27 @@ export default MemberBenefits = () => {
     return (
         <ScrollView style={{ backgroundColor: HiFiColors.Accent, }} contentContainerStyle={{ paddingVertical: 20 }}>
             <View>
-                <View style={styles.imageContainer}>
-                    <Image source={require('../../../assets/images/member-benefits/categories/adidas.png')} />
+                <View style={styles.imageRow}>
+                    <View style={styles.imageContainer1}>
+                        <Image style={styles.imageView} resizeMode={'stretch'} source={require('../../../assets/images/member-benefits/8c31e35fbfe0c83a43c165b9134a2047.jpg')} />
+                        <Image style={styles.imageView} resizeMode={'stretch'} source={require('../../../assets/images/member-benefits/5c5a264257f09ce652c6f7531420197d.jpg')} />
+                        <Image style={styles.imageView} resizeMode={'stretch'} source={require('../../../assets/images/member-benefits/b4276329cae57635c4ae0550731805d1.jpg')} />
+                    </View>
+                    <View style={styles.imageContainer2}>
+                        <Image style={styles.imageView} resizeMode={'stretch'} source={require('../../../assets/images/member-benefits/b2a37d28055200c2b7ed1ee99d5229c3.jpg')} />
+                        <Image style={styles.imageView} resizeMode={'stretch'} source={require('../../../assets/images/member-benefits/66e0b7b5e754b7b5fa7ca4eac619841f.jpg')} />
+                        <Image style={styles.imageView} resizeMode={'stretch'} source={require('../../../assets/images/member-benefits/eebc71ff474b7592d66fc2ffe8416b7d.jpg')} />
+                    </View>
+                    <View style={styles.imageContainer3}>
+                        <Image style={styles.imageView} resizeMode={'stretch'} source={require('../../../assets/images/member-benefits/ae0265bcf5f64944ac35825f79149fbe.jpg')} />
+                        <Image style={styles.imageView} resizeMode={'stretch'} source={require('../../../assets/images/member-benefits/90bb7fbc8257b4114ab01a0bbcf91c64.jpg')} />
+                        <Image style={styles.imageView} resizeMode={'stretch'} source={require('../../../assets/images/member-benefits/dcaa341b60cd84b8177fd55f5bcb2293.jpg')} />
+                    </View>
+                    <View style={styles.imageContainer4}>
+                        <Image style={styles.imageView} resizeMode={'stretch'} source={require('../../../assets/images/member-benefits/0ee818a426e850e7df3f97cbd219278b.jpg')} />
+                        <Image style={styles.imageView} resizeMode={'stretch'} source={require('../../../assets/images/member-benefits/4123d4d62d72c79db6d30bccca2f2b7b.jpg')} />
+                        <Image style={styles.imageView} resizeMode={'stretch'} source={require('../../../assets/images/member-benefits/ae0265bcf5f64944ac35825f79149fbe.jpg')} />
+                    </View>
                 </View>
             </View>
             <View style={styles.caption}>
@@ -322,8 +341,35 @@ export default MemberBenefits = () => {
 }
 
 const styles = StyleSheet.create({
-    imageContainer: {
+    imageRow: {
         flexDirection: 'row',
+    },
+    imageView: {
+        width: 100,
+        height: 120,
+        borderRadius: 10,
+        margin: 5,
+    },
+    imageContainer1: {
+        width: Dimensions.get("window").width / 3.5,
+        marginTop: -80,
+        marginLeft: -20,
+        borderRadius: 10,
+    },
+    imageContainer2: {
+        width: Dimensions.get("window").width / 3.5,
+        marginTop: -40,
+        borderRadius: 10,
+    },
+    imageContainer3: {
+        width: Dimensions.get("window").width / 3.5,
+        marginTop: -50,
+        borderRadius: 10,
+    },
+    imageContainer4: {
+        width: Dimensions.get("window").width / 3.5,
+        marginTop: -30,
+        borderRadius: 10,
     },
     caption: {
         paddingLeft: 10,
