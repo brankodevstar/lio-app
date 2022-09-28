0af6ca58f6958621df528fcfbcfb760343c0edd5
brankodevstar <kyles902619@gmail.com>
2022-09-28 12:21:51 -0700

    c

---
+++ src/service/investments.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import APIServer from "./APIService";
+import { ADMIN_API_URL } from '@env';
+
+const list = async (param) => {
+    console.log('android ======== ', param);
+    const response = await APIServer.get('investments', { params: param });
+    return response;
+}
+
+const getById = async (id) => {
+    const response = await APIServer.get(`investments/${id}`);
+    return response;
+}
+
+export default {
+    list,
+    getById
+}
